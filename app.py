690bba4fb2d93e7015fbb63d922e4da6db363ffe
Eli Ribble <eli@authentise.com>
2017-02-16 17:54:35 -0700

    Add the ability to delete tasks
    I didn't test it very well though...

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 app = Flask(__name__)
 
 tasks = []
 @app.route('/')
 def index():
     return render_template('tasks.html', tasks=tasks)
+
+@app.route('/delete/<i>/')
+def delete(i):
+    del tasks[int(i)]
+    return redirect('/')
 
 def main():
     tasks.append('task 1')
