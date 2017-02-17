1b7bd034e6a4942e76c26d0218d4438a0ffed5fb
Eli Ribble <eli@authentise.com>
2017-02-16 17:45:24 -0700

    Show the list of tasks
    We generate the list so we have something to show, but it's nowhere near
    an actual 'database'. Also updated the name of the template to be more
    fitting of what we're doing with it

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+tasks = []
 @app.route('/')
-def hello_world():
-    return render_template('hello.html')
+def index():
+    return render_template('tasks.html', tasks=tasks)
 
 def main():
+    tasks.append('task 1')
+    tasks.append('task 2')
     app.run('localhost', 8000)
 
 if __name__ == '__main__':
